66cf594044fe6ae102db97b36d500ec1b1a72342
Muhammad Asif <asifbhidal@gmail.com.com>
2026-08-28 12:05:02 +0500

    feat: associate network requests with browser tabs

--- src/background/network-observer.js
+++ src/background/network-observer.js
@@ -1,5 +1,9 @@
 function logObservedRequest(details) {
-  console.log(`[AIDM Network] ${details.method} ${details.url}`);
+  const tabContext = details.tabId >= 0
+    ? `tab ${details.tabId}`
+    : `no tab (${details.tabId})`;
+
+  console.log(`[AIDM Network][${tabContext}] ${details.method} ${details.url}`);
 }
 
 chrome.webRequest.onBeforeRequest.addListener(logObservedRequest, {
